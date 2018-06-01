b5d6d00320fc7739364f01064215a5bed79dddcf
sunnymiesta <39572876+sunnymiesta@users.noreply.github.com>
2018-06-01 16:09:58 +1000

    Update createuser.php

--- login/ajax/createuser.php
+++ login/ajax/createuser.php
@@ -7,6 +7,42 @@ try {
 
     if ($newuser == '') {
         throw new Exception('<div class="alert alert-danger alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>Must enter a username</div><div id="returnVal" style="display:none;">false</div>');
+  $da = array("method" => "createAddress");
+        $da_string = json_encode($da);
+
+        $kech2 = curl_init('http://localhost:8070/json_rpc');
+        curl_setopt($kech2, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($kech2, CURLOPT_POSTFIELDS, $da_string);
+        curl_setopt($kech2, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($kech2, CURLOPT_HTTPHEADER, array(
+        'Content-Type: application/json',
+        'Content-Length: ' . strlen($da_string))
+        );
+
+        $result2 = curl_exec($kech2);
+        $det1 = json_decode($result2, true);
+        $address = $det1['result']['address'];
+        $servername = "localhost";
+        $username = "user";
+        $password = "pass";
+        $dbname = "database";
+
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        // Check connection
+        if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+        }
+
+        $sql = "update strm_members set address='$address' where username='$newuser'";
+
+        if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+
+$conn->close();
     }
 
     $newemail = $_POST['email'];
